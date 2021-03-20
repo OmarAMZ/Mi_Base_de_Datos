a357445a21135682655f8096003e00b04b0b50c7
OmarAMZ <s16120319@alumnos.itsur.edu.mx>
2021-03-20 16:10:45 -0600

    v2

--- app/src/main/java/com/example/mi_base_de_datos/MainActivity.java
+++ app/src/main/java/com/example/mi_base_de_datos/MainActivity.java
@@ -3,8 +3,17 @@ package com.example.mi_base_de_datos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    Button btnCrear, btnBuscar;
+    EditText txtFiltro;
+    SimpleCursorAdapter adp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
